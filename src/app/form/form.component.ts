8fa4882bb97fa78ebddc9864e43d423fa75ff246
jaisurya <jsurya787@gmail.com>
2023-03-01 16:49:51 +0530

    first ui

---
+++ src/app/form/form.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent {
+  loginform =new FormGroup({
+    Firstname:new FormControl('',[Validators.required,Validators.minLength(50) ]),
+    Lastname:new FormControl('',[Validators.required,Validators.minLength(50) ]),
+    EmailAddress:new FormControl('',[Validators.required,Validators.minLength(254) ]),
+    Phonenumber:new FormControl('',[Validators.required,Validators.minLength(10) ]),
+    OrganizationName:new FormControl('',[Validators.required,Validators.minLength(1000) ]),
+    OrganizationAddress:new FormControl('',[Validators.required,Validators.minLength(1000) ]),
+    password:new FormControl('',[Validators.required,Validators.minLength(16) ])
+  })
+  Submit(){
+    console.warn(this.loginform.value)
+  }
+  get Firstname(){
+    return this.loginform.get('Firstname')
+  }
+  get Lastname(){
+    return this.loginform.get('Lastname')
+  }
+  get EmailAddress(){
+    return this.loginform.get('EmailAddress')
+  }
+  get Phonenumber(){
+    return this.loginform.get('Phonenumber')
+  }
+  get OrganizationName(){
+    return this.loginform.get('OrganizationName')
+  }
+  get OrganizationAddress(){
+    return this.loginform.get('OrganizationAddress')
+  }
+  get password(){
+    return this.loginform.get('password')
+  }
+  set(){
+     this.loginform.reset()
+  }
+}
